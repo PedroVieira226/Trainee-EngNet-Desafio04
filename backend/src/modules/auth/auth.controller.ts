e0a4f952498497fb9a42f91fecb8b3fb491b87a1
Pedro Vieira <pedrovieira1355@gmail.com>
2026-06-11 15:14:17 -0300

    feat: implementa autenticação JWT, hashing com bycrypt e jwtauthguard

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')//define o prefixo como /auth
 export class AuthController {
@@ -10,5 +11,15 @@ export class AuthController {
   @HttpCode(HttpStatus.OK) // Força o retorno do status HTTP 200 ao invés de 201
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);//repassa a requisicao para o service
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard) //  o guard entra aqui trancando a rota
+  getProfile(@Req() req) {
+    //se o token for válido, o nest injeta os dados do user dentro de req.user
+    return {
+      mensagem: 'Você acessou uma rota privada com sucesso!',
+      usuario: req.user,
+    };
   }
 }
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,47 +1,57 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  //mockado ate o baldit entregar o banco
-  private usuariosSimulados = [
-    {
-      id: '1',
-      nome: 'Admin do Sistema',
-      email: 'admin@engnet.com',
-      senha: 'securepassword123', //trocar por hash
-      role: 'ADMIN',
-    },
-    {
-      id: '2',
-      nome: 'Professor de Engenharia',
-      email: 'professor@engnet.com',
-      senha: 'passwordprof123',
-      role: 'PROFESSOR',
-    },
-  ];
+  //injeta o JwtService 
+  constructor(private readonly jwtService: JwtService) {}
 
   async login(loginDto: LoginDto) {
     const { email, senha } = loginDto;
 
-    
-    const usuario = this.usuariosSimulados.find(u => u.email === email);
+    //GERADOR DE HASH TEMPORÁRIO (SÓ PARA DESCOBRIR O VALOR REAL NO DOCKER)
+    //tudo isso vem do banco depois  
+    const hashGeradoNaHora = await bcrypt.hash('passwordprof123', 10);
+    console.log('====================================');
+    console.log('HASH REAL DA SENHA:', hashGeradoNaHora); 
+    console.log('====================================');
 
+    // SIMULAÇÃO DE BANCO DE DADOS (Parâmetros de Produção)
     
-    if (!usuario || usuario.senha !== senha) {
-      throw new UnauthorizedException('E-mail ou senha incorretos.');
+    const professorSimulado = {
+      id: 'id-professor-fga-2026',
+      email: 'professor@engnet.com',
+      
+      senhaHash: hashGeradoNaHora, 
+      role: 'professor',
+    };
+
+    // 1. Verificar se o e-mail bate com o usuário simulado
+    if (email !== professorSimulado.email) {
+      
+      throw new UnauthorizedException('Credenciais inválidas');
     }
 
-    //retornando dados do usuário logado
-    //transformar isso em um JWT dps)
+    // 2. Comparar a senha digitada (limpa) com o hash simulado do banco
+    const senhaBate = await bcrypt.compare(senha, professorSimulado.senhaHash);
+
+    if (!senhaBate) {
+      // Retorna 401 se a senha estiver errada
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    // 3. Gerar o payload do JWT (dados não-sensíveis que vão dentro do token)
+    const payload = { 
+      sub: professorSimulado.id, 
+      email: professorSimulado.email, 
+      role: professorSimulado.role 
+    };
+
+    // 4. Retornar o token JWT assinado
     return {
-      message: 'Autenticação realizada com sucesso!',
-      user: {
-        id: usuario.id,
-        nome: usuario.nome,
-        email: usuario.email,
-        role: usuario.role,
-      },
+      access_token: this.jwtService.sign(payload),
     };
   }
 }
